606ecbf3f269e8422adc7d938b61d3dc7336f044
adnprogramador <adnprogramador@gmail.com>
2015-07-22 01:05:16 +0200

    Rama con créditos

--- app.js
+++ app.js
@@ -11,11 +11,6 @@ process.title = 'quiz';
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 
-/*app.get('/',function(req,res,next){
-  res.render('index.ejs') 
-  // -> render layout.ejs with index.ejs as `body`.
-});*/
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
