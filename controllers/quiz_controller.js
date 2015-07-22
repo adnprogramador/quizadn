dde1eb10138ad987ee5d680cab38b74afc78711d
adnprogramador <adnprogramador@gmail.com>
2015-07-22 21:06:30 +0200

    Despliegue en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,3 +1,4 @@
+/*
 //GET /quizes/question
 
 exports.question = function (req, res){
@@ -13,9 +14,104 @@ exports.answer = function (req, res){
 		res.render('quizes/answer', {title: 'Respuesta', respuesta: '¡Incorrecto!'});
 	}
 };
+*/
+
+var models = require('../models/models.js');
+/*
+//GET /quizes/question
+
+exports.question = function (req, res){
+	models.Quiz.findAll().then(
+		function(quiz){
+			res.render('quizes/question', { title: 'Pregunta', pregunta: quiz[0].pregunta});
+		})
+};
+//GET /quizes/answer
+exports.answer = function (req, res){ 
+	models.Quiz.findAll().then(
+		function(quiz){
+			if(req.query.respuesta === quiz[0].respuesta){
+				res.render('quizes/answer', {title: 'Respuesta', respuesta: '¡Correcto!'});
+			}else{
+				res.render('quizes/answer', {title: 'Respuesta', respuesta: '¡Incorrecto!'});
+			}
+		})
+};
+*/
+// Autoload :id - factoriza el código si ruta incluye :quizId
+exports.load = function (req, res, next, quizId){
+	models.Quiz.find({
+where: {
+id: Number(quizId)
+},
+}).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			}else{next(new Error('No existe quizId='+quizId));}
+		}
+	).catch(function(error){next(error);});
+};
+
+//GET /quizes
+exports.index = function (req, res){
+	models.Quiz.findAll().then(
+		function(quizes){
+			res.render('quizes/index.ejs', { quizes: quizes, errors: []});
+		}
+	).catch(function(error){next(error);});
+};
+
+//GET /quizes/:id
+exports.show = function (req, res){
+	res.render('quizes/show', { quiz: req.quiz, errors: []});
+	
+};
+
+//GET /quizes/:id/answer
+exports.answer = function (req, res){
+	var resultado = '¡Incorrecto!';
+	if(req.query.respuesta === req.quiz.respuesta){
+		resultado = '¡Correcto!';
+	}
+	res.render('quizes/answer', { 
+		quiz: req.quiz, 
+		respuesta: resultado, 
+		errors: [] 
+	}
+	);
+};
 
 //GET /author
 
 exports.author = function (req, res){
 	res.render('author', {title: 'Créditos', autor: 'Adrián Luis Blanco Calleja'});
+};
+
+//GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( //crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+    res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+//GET /quizes/create
+exports.create = function(req, res) {
+    var quiz = models.Quiz.build( req.body.quiz );
+
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta", "UserId"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  ).catch(function(error){next(error)});
 };
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,21 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// GET Página de entrada (home page).
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-router.get('/author', quizController.author);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
+// Definición de rutas de /quizes
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', 							quizController.index);
+router.get('/quizes/quizId(\\d+)', 			quizController.show);
+router.get('/quizes/quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', 						quizController.new);
+router.get('/quizes/create', 					quizController.create);
 
 module.exports = router;
